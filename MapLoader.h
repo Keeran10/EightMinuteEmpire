671816aa2b99e8b67aa851b04594b39a1c670179
Keeran10 <keeran.r10@gmail.com>
2019-11-17 22:18:32 -0500

    comment out buggy code to have a running app

--- MapLoader.h
+++ MapLoader.h
@@ -4,7 +4,7 @@
 class MapLoader
 {
 private:
-	static Map* map;
+	Map* map;
 	bool isValid;
 	
 public:
@@ -13,7 +13,7 @@ public:
 	~MapLoader();
 	bool LoadMap(Map* map, string file_path);
 	inline bool getIsValid() { return isValid; }
-	static Map* GetMap(); //changed this to static to get map in SingletonDriver.cpp 
+	Map* GetMap(); //changed this to static to get map in SingletonDriver.cpp 
 };
 
 inline Map* MapLoader::GetMap() { return map; }
--- SingletonDriver.cpp
+++ SingletonDriver.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 void SingletonDriver(){
     cout<< "Please select the first map"<<endl;
-    Map* firstMap = MapLoader::GetMap();
+    //Map* firstMap = MapLoader::GetMap();
 
     cout<< "Please select the second map"<<endl;
-    Map *secondMap = MapLoader::GetMap();
+    //Map* secondMap = MapLoader::GetMap();
 
-    cout<<"The pointer to map 1 is: "<<firstMap<<endl;
-    cout<<"The pointer to map 2 is: "<<secondMap<<endl;
+    //cout<<"The pointer to map 1 is: "<<firstMap<<endl;
+    //cout<<"The pointer to map 2 is: "<<secondMap<<endl;
 
 }
